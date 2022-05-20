198a552e8820be6dd0815ca2a9f1ade9ce3dd1b1
Thomas Rittson <trittson@bitwarden.com>
2022-05-20 17:17:37 +1000

    Move ngOnInit from super class to base class

--- apps/desktop/src/app/modules/vault-filter/vault-filter.component.ts
+++ apps/desktop/src/app/modules/vault-filter/vault-filter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { VaultFilterComponent as BaseVaultFilterComponent } from "jslib-angular/modules/vault-filter/vault-filter.component";
 
@@ -6,4 +6,10 @@ import { VaultFilterComponent as BaseVaultFilterComponent } from "jslib-angular/
   selector: "app-vault-filter",
   templateUrl: "vault-filter.component.html",
 })
-export class VaultFilterComponent extends BaseVaultFilterComponent {}
+export class VaultFilterComponent extends BaseVaultFilterComponent implements OnInit {
+  async ngOnInit() {
+    await super.ngOnInit(); 
+    this.folders = await this.vaultFilterService.buildFolders();
+    this.collections = await this.vaultFilterService.buildCollections();
+  }
+}
