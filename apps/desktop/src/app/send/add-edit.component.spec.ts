95ec187fb9c283cb0325cf1d5711b08bed8d9a6d
Robyn MacCallum <robyntmaccallum@gmail.com>
2022-06-07 16:55:37 -0400

    Destop tests start

---
+++ apps/desktop/src/app/send/add-edit.component.spec.ts
@@ -0,0 +1,42 @@
+// import { DatePipe } from "@angular/common";
+// import { EnvironmentService } from "jslib-common/abstractions/environment.service";
+// import { I18nService } from "jslib-common/abstractions/i18n.service";
+// import { LogService } from "jslib-common/abstractions/log.service";
+// import { MessagingService } from "jslib-common/abstractions/messaging.service";
+// import { PlatformUtilsService } from "jslib-common/abstractions/platformUtils.service";
+// import { PolicyService } from "jslib-common/abstractions/policy.service";
+// import { SendService } from "jslib-common/abstractions/send.service";
+// import { StateService } from "jslib-common/abstractions/state.service";
+// import { AddEditComponent } from "./add-edit.component";
+
+// describe("MyFeatureComponent", () => {
+//   let component: AddEditComponent;
+
+//   let i18nService = {} as I18nService;
+//   let platformUtilsService = {} as PlatformUtilsService;
+//   let environmentService = {} as EnvironmentService;
+//   let datePipe = new DatePipe("en");
+//   let sendService = {} as SendService;
+//   let stateService = {} as StateService;
+//   let messagingService = {} as MessagingService;
+//   let policyService = {} as PolicyService;
+//   let logService = {} as LogService;
+
+//   beforeEach(() => {
+//     component = new AddEditComponent(
+//       i18nService,
+//       platformUtilsService,
+//       environmentService,
+//       datePipe,
+//       sendService,
+//       stateService,
+//       messagingService,
+//       policyService,
+//       logService
+//     );
+//   });
+
+//   it("should create", () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
