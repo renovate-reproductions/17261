843cf86c81251fd6a35819d1de4bc99b31384710
Thomas Rittson <trittson@bitwarden.com>
2022-06-20 15:19:02 +1000

    Parse encArrayBuffer using gettings instead of ctor

--- libs/common/src/models/domain/encArrayBuffer.ts
+++ libs/common/src/models/domain/encArrayBuffer.ts
@@ -1,16 +1,36 @@
 import { EncryptionType } from "@bitwarden/common/enums/encryptionType";
 
 export class EncArrayBuffer {
-  ctBytes: Uint8Array = null; // cipher text bytes
-  ivBytes: Uint8Array = null;
-  macBytes: Uint8Array = null;
-  encryptionType: EncryptionType = null;
+  constructor(public buffer: ArrayBuffer) {}
 
-  constructor(public buffer: ArrayBuffer) {
+  get encryptionType(): EncryptionType {
+    return this.parseBytes?.encryptionType;
+  }
+
+  get ivBytes(): Uint8Array {
+    return this.parseBytes?.ivBytes;
+  }
+
+  get macBytes(): Uint8Array {
+    return this.parseBytes?.macBytes;
+  }
+
+  // Cipher Text
+  get ctBytes(): Uint8Array {
+    return this.parseBytes?.ctBytes;
+  }
+
+  private get parseBytes(): {
+    encryptionType: EncryptionType;
+    ivBytes: Uint8Array;
+    macBytes: Uint8Array;
+    ctBytes: Uint8Array;
+  } {
     const encBytes = new Uint8Array(this.buffer);
-    this.encryptionType = encBytes[0];
 
-    switch (this.encryptionType) {
+    const encryptionType = encBytes[0];
+
+    switch (encryptionType) {
       case EncryptionType.AesCbc128_HmacSha256_B64:
       case EncryptionType.AesCbc256_HmacSha256_B64:
         if (encBytes.length <= 49) {
@@ -18,19 +38,26 @@ export class EncArrayBuffer {
           return null;
         }
 
-        this.ivBytes = encBytes.slice(1, 17);
-        this.macBytes = encBytes.slice(17, 49);
-        this.ctBytes = encBytes.slice(49);
-        break;
+        return {
+          encryptionType: encryptionType,
+          ivBytes: encBytes.slice(1, 17),
+          macBytes: encBytes.slice(17, 49),
+          ctBytes: encBytes.slice(49),
+        };
+
       case EncryptionType.AesCbc256_B64:
         if (encBytes.length <= 17) {
           // 1 + 16 + ctLength
           return null;
         }
 
-        this.ivBytes = encBytes.slice(1, 17);
-        this.ctBytes = encBytes.slice(17);
-        break;
+        return {
+          encryptionType: encryptionType,
+          ivBytes: encBytes.slice(1, 17),
+          macBytes: null,
+          ctBytes: encBytes.slice(17),
+        };
+
       default:
         return null;
     }
