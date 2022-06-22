feb3ea4e784138ce415e05d13131104bdb3389e1
Hinton <hinton@users.noreply.github.com>
2022-06-22 12:31:29 +0200

    Fix desktop

--- apps/desktop/src/app/app.component.ts
+++ apps/desktop/src/app/app.component.ts
@@ -193,7 +193,7 @@ export class AppComponent implements OnInit {
             await this.reloadProcess();
             break;
           case "reloadProcess":
-            window.location.reload(true);
+            (window.location as any).reload(true);
             break;
           case "syncStarted":
             break;
