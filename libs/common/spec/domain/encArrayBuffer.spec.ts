71df1bdf360e601dd7734fe5e59fd045a776d109
Thomas Rittson <trittson@bitwarden.com>
2022-06-30 16:22:20 +1000

    Add coverage for all encTypes

--- libs/common/spec/domain/encArrayBuffer.spec.ts
+++ libs/common/spec/domain/encArrayBuffer.spec.ts
@@ -5,8 +5,10 @@ import { makeStaticByteArray } from "../utils";
 
 describe("encArrayBuffer", () => {
   describe("parses the buffer", () => {
-    it("with AesCbc128_HmacSha256_B64", () => {
-      const encType = EncryptionType.AesCbc128_HmacSha256_B64;
+    test.each([
+      [EncryptionType.AesCbc128_HmacSha256_B64, "AesCbc128_HmacSha256_B64"],
+      [EncryptionType.AesCbc256_HmacSha256_B64, "AesCbc256_HmacSha256_B64"],
+    ])("with %c%s", (encType: EncryptionType) => {
       const iv = makeStaticByteArray(16, 10);
       const mac = makeStaticByteArray(32, 20);
       const cipherText = makeStaticByteArray(20, 30);
